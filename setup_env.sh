fdf2ae3446b3ba075ce3a203a9cdd8e72b0efc27
Chris Allen <chris@apaxsoftware.com>
2022-07-29 09:37:37 -0400

    Renamed DB_PWD to DB_PASSWORD

--- setup_env.sh
+++ setup_env.sh
@@ -6,7 +6,7 @@ set -e
 export RSA_PRIVATE_KEY=$(echo $RSA_PRIVATE_KEY | base64 -d)
 
 # Secret compositions
-export DATABASE_URL=postgres://${DB_USER}:${DB_PWD}@${DB_HOST}:5432/${DB_NAME}
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_NAME}
 
 # Run gunicorn command
 exec "$@"
